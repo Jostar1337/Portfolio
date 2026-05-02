35efb58992af358cf1ca52a6d364bbdeab28816a
Jojo1337 <kamo.essen@gmail.com>
2026-05-03 01:10:45 +0200

    add volume box & fix random bugs

--- static/js/music-player.js
+++ static/js/music-player.js
@@ -45,6 +45,10 @@
                     '<svg viewBox="0 0 24 24" fill="currentColor"><path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/></svg>' +
                 '</button>' +
             '</div>' +
+            '<div class="volume-container">' +
+                '<svg class="volume-icon" viewBox="0 0 24 24" fill="currentColor"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>' +
+                '<input type="range" id="player-volume" min="0" max="100" value="100" class="volume-slider">' +
+            '</div>' +
         '</div>';
     }
 
@@ -257,6 +261,37 @@
         if (playBtn) playBtn.addEventListener('click', togglePlay);
         if (nextBtn) nextBtn.addEventListener('click', nextTrack);
         if (prevBtn) prevBtn.addEventListener('click', prevTrack);
+
+        var volSlider = document.getElementById('player-volume');
+        var volIcon = document.querySelector('.volume-icon');
+        if (volSlider) {
+            volSlider.addEventListener('input', function(e) {
+                var val = e.target.value;
+                if (ytPlayer && playerReady) {
+                    ytPlayer.setVolume(val);
+                }
+                e.target.style.background = 'linear-gradient(to right, #fff ' + val + '%, rgba(255, 255, 255, 0.1) ' + val + '%)';
+            });
+            volSlider.style.background = 'linear-gradient(to right, #fff 100%, rgba(255, 255, 255, 0.1) 100%)';
+        }
+
+        if (volIcon && volSlider) {
+            volIcon.style.cursor = 'pointer';
+            volIcon.addEventListener('click', function() {
+                if (ytPlayer && playerReady) {
+                    if (ytPlayer.isMuted()) {
+                        ytPlayer.unMute();
+                        volSlider.value = ytPlayer.getVolume() || 100;
+                        volIcon.style.opacity = '1';
+                    } else {
+                        ytPlayer.mute();
+                        volSlider.value = 0;
+                        volIcon.style.opacity = '0.3';
+                    }
+                    volSlider.dispatchEvent(new Event('input'));
+                }
+            });
+        }
 
         adjustHeroLayout();
         loadYouTubeAPI();
